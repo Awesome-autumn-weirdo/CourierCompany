ccc56ec21034f7acb8d37e5df6a7059a8235153e
Валерия Качигина <vfvjxrf0508@outlook.com>
2024-08-03 19:47:28 +0700

    все еще колдую

--- CourierCompany/Simulation.cs
+++ CourierCompany/Simulation.cs
@@ -1,156 +1,156 @@
-﻿using CourierCompany;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Simulation : ISimulation
+namespace CourierCompany
 {
-    private readonly List<Courier> couriers;
-    private readonly List<Order> orders;
-
-    private readonly int numCouriers;
-    private readonly double simulationTime;
-    private readonly double averageCourierSpeed;
-    private readonly double meanTimeBetweenOrders;
-    private readonly double deltaT;
-    private readonly double maxDistance;
-
-    private int totalOrders = 0;
-    private int completedOrders = 0;
-    private int failedOrders = 0;
-    private double totalWaitingTime = 0;
-
-    public Simulation(int numCouriers, double simulationTime, double averageCourierSpeed, double meanTimeBetweenOrders, double deltaT, double maxDistance)
+    public class Simulation : ISimulation
     {
-        this.numCouriers = numCouriers;
-        this.simulationTime = simulationTime;
-        this.averageCourierSpeed = averageCourierSpeed;
-        this.meanTimeBetweenOrders = meanTimeBetweenOrders;
-        this.deltaT = deltaT;
-        this.maxDistance = maxDistance;
+        private readonly List<Courier> couriers;
+        private readonly List<Order> orders;
 
-        couriers = new List<Courier>(numCouriers);
-        orders = new List<Order>();
-    }
+        private readonly int numCouriers;
+        private readonly double simulationTime;
+        private readonly double averageCourierSpeed;
+        private readonly double meanTimeBetweenOrders;
+        private readonly double deltaT;
+        private readonly double maxDistance;
 
-    public void Run()
-    {
-        InitializeCouriers();
-        SimulateDeliveries();
-    }
+        private int totalOrders = 0;
+        private int completedOrders = 0;
+        private int failedOrders = 0;
+        private double totalWaitingTime = 0;
 
-    public SimulationResult GetResult()
-    {
-        double averageWaitingTime = StatisticsUtils.CalculateAverageWaitingTime(orders, completedOrders);
-        double averageIdleTime = StatisticsUtils.CalculateAverageIdleTime(couriers);
-        double averageLoad = StatisticsUtils.CalculateAverageCourierLoad(couriers);
-        double averageDeliveryTime = orders.Any() ? orders.Average(o => o.DeliveryTime - o.ArrivalTime) : 0;
-        double averageDistance = StatisticsUtils.CalculateAverageDistance(orders);
-
-        return new SimulationResult
+        public Simulation(int numCouriers, double simulationTime, double averageCourierSpeed, double meanTimeBetweenOrders, double deltaT, double maxDistance)
         {
-            TotalOrders = totalOrders,
-            CompletedOrders = completedOrders,
-            FailedOrders = failedOrders,
-            AverageWaitingTime = averageWaitingTime,
-            AverageIdleTime = averageIdleTime,
-            AverageCourierLoad = averageLoad,
-            AverageDeliveryTime = averageDeliveryTime,
-            AverageDistance = averageDistance
-        };
-    }
+            this.numCouriers = numCouriers;
+            this.simulationTime = simulationTime;
+            this.averageCourierSpeed = averageCourierSpeed;
+            this.meanTimeBetweenOrders = meanTimeBetweenOrders;
+            this.deltaT = deltaT;
+            this.maxDistance = maxDistance;
 
-    private void InitializeCouriers()
-    {
-        couriers.Clear();
-        for (int i = 0; i < numCouriers; i++)
-        {
-            couriers.Add(new Courier());
+            couriers = new List<Courier>(numCouriers);
+            orders = new List<Order>();
         }
-    }
 
-    private void SimulateDeliveries()
-    {
-        double currentTime = 0;
-        double timeToNextOrder = RandomUtils.GenerateExponentialRandom(1.0 / (meanTimeBetweenOrders / 60.0));
-
-        while (currentTime < simulationTime)
+        public void Run()
         {
-            foreach (var courier in couriers)
+            InitializeCouriers();
+            SimulateDeliveries();
+        }
+
+        public SimulationResult GetResult()
+        {
+            double averageWaitingTime = StatisticsUtils.CalculateAverageWaitingTime(orders, completedOrders);
+            double averageIdleTime = StatisticsUtils.CalculateAverageIdleTime(couriers);
+            double averageLoad = StatisticsUtils.CalculateAverageCourierLoad(couriers);
+            double averageDeliveryTime = orders.Any() ? orders.Average(o => o.DeliveryTime - o.ArrivalTime) : 0;
+            double averageDistance = StatisticsUtils.CalculateAverageDistance(orders);
+
+            return new SimulationResult
             {
-                for (int i = courier.AssignedOrders.Count - 1; i >= 0; i--)
+                TotalOrders = totalOrders,
+                CompletedOrders = completedOrders,
+                FailedOrders = failedOrders,
+                AverageWaitingTime = averageWaitingTime,
+                AverageIdleTime = averageIdleTime,
+                AverageCourierLoad = averageLoad,
+                AverageDeliveryTime = averageDeliveryTime,
+                AverageDistance = averageDistance
+            };
+        }
+
+        private void InitializeCouriers()
+        {
+            couriers.Clear();
+            for (int i = 0; i < numCouriers; i++)
+            {
+                couriers.Add(new Courier());
+            }
+        }
+
+        private void SimulateDeliveries()
+        {
+            double currentTime = 0;
+            double timeToNextOrder = RandomUtils.GenerateExponentialRandom(1.0 / (meanTimeBetweenOrders / 60.0));
+
+            while (currentTime < simulationTime)
+            {
+                foreach (var courier in couriers)
                 {
-                    var order = courier.AssignedOrders[i];
-                    if (order.DeliveryTime <= currentTime)
+                    foreach (var order in courier.AssignedOrders.ToList())
                     {
-                        completedOrders++;
-                        double waitingTime = order.DeliveryTime - order.ArrivalTime;
-                        totalWaitingTime += waitingTime;
-                        courier.CompleteOrder(order, currentTime);
+                        if (order.DeliveryTime <= currentTime)
+                        {
+                            completedOrders++;
+                            double waitingTime = order.DeliveryTime - order.ArrivalTime;
+                            totalWaitingTime += waitingTime;
+                            courier.CompleteOrder(order, currentTime);
+                        }
                     }
                 }
+
+                if (timeToNextOrder <= currentTime)
+                {
+                    double distance = RandomUtils.GenerateUniformRandom(1.0, maxDistance);
+                    Order newOrder = new Order(currentTime, distance);
+                    orders.Add(newOrder);
+                    totalOrders++;
+
+                    AssignOrderToCourier(newOrder);
+
+                    timeToNextOrder += RandomUtils.GenerateExponentialRandom(1.0 / (meanTimeBetweenOrders / 60.0));
+                }
+
+                currentTime += deltaT;
             }
 
-            if (timeToNextOrder <= currentTime)
+            foreach (var courier in couriers)
             {
-                double distance = RandomUtils.GenerateUniformRandom(1.0, maxDistance);
-                Order newOrder = new Order(currentTime, distance);
-                orders.Add(newOrder);
-                totalOrders++;
-
-                AssignOrderToCourier(newOrder);
-
-                timeToNextOrder += RandomUtils.GenerateExponentialRandom(1.0 / (meanTimeBetweenOrders / 60.0));
-            }
-
-            currentTime += deltaT;
-        }
-
-        foreach (var courier in couriers)
-        {
-            failedOrders += courier.AssignedOrders.Count;
-        }
-    }
-
-    private void AssignOrderToCourier(Order newOrder)
-    {
-        Courier leastLoadedCourier = couriers.OrderBy(c => c.CurrentLoad).FirstOrDefault();
-        if (leastLoadedCourier == null)
-        {
-            failedOrders++;
-            return;
-        }
-
-        leastLoadedCourier.AssignOrder(newOrder);
-
-        double deliveryTime = newOrder.Distance / averageCourierSpeed * 60.0;
-        newOrder.SetDeliveryTime(newOrder.ArrivalTime + deliveryTime);
-    }
-
-    // Добавляем свойства с сеттерами для доступа к спискам заказов и курьеров
-    public List<Courier> Couriers
-    {
-        get => couriers;
-        set
-        {
-            // Заменить текущий список курьеров новыми данными
-            couriers.Clear();
-            if (value != null)
-            {
-                couriers.AddRange(value);
+                failedOrders += courier.AssignedOrders.Count;
             }
         }
-    }
 
-    public List<Order> Orders
-    {
-        get => orders;
-        set
+        private void AssignOrderToCourier(Order newOrder)
         {
-            // Заменить текущий список заказов новыми данными
-            orders.Clear();
-            if (value != null)
+            Courier leastLoadedCourier = couriers.OrderBy(c => c.CurrentLoad).FirstOrDefault();
+            if (leastLoadedCourier == null)
             {
-                orders.AddRange(value);
+                failedOrders++;
+                return;
+            }
+
+            leastLoadedCourier.AssignOrder(newOrder);
+
+            double deliveryTime = newOrder.Distance / averageCourierSpeed * 60.0;
+            newOrder.SetDeliveryTime(newOrder.ArrivalTime + deliveryTime);
+        }
+
+        // Добавляем свойства с сеттерами для доступа к спискам заказов и курьеров
+        public List<Courier> Couriers
+        {
+            get => couriers;
+            set
+            {
+                couriers.Clear();
+                if (value != null)
+                {
+                    couriers.AddRange(value);
+                }
+            }
+        }
+
+        public List<Order> Orders
+        {
+            get => orders;
+            set
+            {
+                orders.Clear();
+                if (value != null)
+                {
+                    orders.AddRange(value);
+                }
             }
         }
     }
--- CourierCompany/Statistic.cs
+++ CourierCompany/Statistic.cs
@@ -3,17 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class StatisticsUtils
+
+namespace CourierCompany
 {
-    public static double CalculateAverageWaitingTime(IEnumerable<Order> orders, int completedOrders)
-        => completedOrders > 0 ? orders.Where(o => o.DeliveryTime <= DateTime.Now.Ticks).Average(o => o.DeliveryTime - o.ArrivalTime) : 0;
+    public static class StatisticsUtils
+    {
+        public static double CalculateAverageWaitingTime(IEnumerable<Order> orders, int completedOrders)
+            => completedOrders > 0
+                ? orders.Where(o => o.DeliveryTime > o.ArrivalTime) // Учитываем только завершенные заказы
+                        .Average(o => o.DeliveryTime - o.ArrivalTime)
+                : 0;
 
-    public static double CalculateAverageIdleTime(IEnumerable<Courier> couriers)
-        => couriers.Any() ? couriers.Average(c => c.IdleTime) : 0;
+        public static double CalculateAverageIdleTime(IEnumerable<Courier> couriers)
+            => couriers.Any() ? couriers.Average(c => c.IdleTime) : 0;
 
-    public static double CalculateAverageCourierLoad(IEnumerable<Courier> couriers)
-        => couriers.Any() ? couriers.Sum(c => c.TotalOrdersAssigned) / (double)couriers.Count() : 0;
+        public static double CalculateAverageCourierLoad(IEnumerable<Courier> couriers)
+            => couriers.Any() ? couriers.Average(c => c.TotalOrdersAssigned) : 0;
 
-    public static double CalculateAverageDistance(IEnumerable<Order> orders)
-        => orders.Any() ? orders.Average(o => o.Distance) : 0;
+        public static double CalculateAverageDistance(IEnumerable<Order> orders)
+            => orders.Any() ? orders.Average(o => o.Distance) : 0;
+    }
 }
+
+
